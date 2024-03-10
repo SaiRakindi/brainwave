e5afd6498ad611a3be12ddaa365cc02c2e404133
SaiRakindi <sai.rakindi3@gmail.com>
2024-03-10 21:21:50 +0530

    feat: main hero section implementation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,14 @@ import { curve, heroBackground } from "../assets";
 import Button from "./Button";
 import Section from "./Section";
 import { robot } from "../assets";
+import { useRef } from "react";
+import { ScrollParallax } from "react-just-parallax";
+import { heroIcons } from "../constants";
+import { BackgroundCircles, Gradient } from "./design/Hero";
 
 const Hero = () => {
+  const parallaxRef = useRef(null);
+
   return (
     <Section
       className="pt-[12rem] -mt-[5.25rem]"
@@ -12,7 +18,7 @@ const Hero = () => {
       customPaddings
       id="hero"
     >
-      <div className="container relative">
+      <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb:[6rem]">
           <h1 className="h1 mb-6">
             Explore the Possibilities with AI Chatting with
@@ -47,8 +53,25 @@ const Hero = () => {
                     height={490}
                     alt="AI"
                   />
+
+                  <ScrollParallax isAbsolutelyPositioned>
+                    <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
+                      {heroIcons.map((heroIcon, index) => (
+                        <li className="p-5" key={index}>
+                          <img
+                            src={heroIcon}
+                            width={24}
+                            height={25}
+                            alt={heroIcon}
+                          />
+                        </li>
+                      ))}
+                    </ul>
+                  </ScrollParallax>
                 </div>
               </div>
+
+              <Gradient />
             </div>
 
             <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] lg:-top[138%] lg:-top-[104%]">
@@ -60,6 +83,8 @@ const Hero = () => {
                 alt="hero"
               />
             </div>
+
+            <BackgroundCircles />
           </div>
         </div>
       </div>
