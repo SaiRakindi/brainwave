40e6272f20bab83339f526c36a0c24631ea9fe9c
SaiRakindi <sai.rakindi3@gmail.com>
2024-03-19 22:41:14 +0530

    style: added styles to the benefits section

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -2,6 +2,8 @@ import { benefits } from "../constants";
 import Heading from "./Heading";
 import Section from "./Section";
 
+import Arrow from "../assets/svg/Arrow";
+
 const Benefits = () => {
   return (
     <Section id="features">
@@ -18,8 +20,21 @@ const Benefits = () => {
               className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
               style={{ backgroundImage: `url(${benefit.backgroundUrl})` }}
             >
-              <div>
-                <h5>{benefit.title}</h5>
+              <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
+                <h5 className="h2 mb-5">{benefit.title}</h5>
+                <p className="body-2 mb-6 text-n-3">{benefit.text}</p>
+                <div className="flex items-center mt-auto">
+                  <img
+                    src={benefit.iconUrl}
+                    width={48}
+                    height={48}
+                    alt={benefit.title}
+                  />
+                  <p className="ml-auto font-code text-sx font-bold text-n-1 uppercase tracking-wider">
+                    Explore more
+                  </p>
+                  <Arrow />
+                </div>
               </div>
             </div>
           ))}
