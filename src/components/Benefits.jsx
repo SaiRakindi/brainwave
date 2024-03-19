3119f3f5aa092b4892fbf7262c15babe31f3e8e9
SaiRakindi <sai.rakindi3@gmail.com>
2024-03-19 22:50:55 +0530

    feat: benefits section ui and functionality implemented

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -4,6 +4,9 @@ import Section from "./Section";
 
 import Arrow from "../assets/svg/Arrow";
 
+import { GradientLight } from "./design/Benefits";
+import ClipPath from "../assets/svg/ClipPath";
+
 const Benefits = () => {
   return (
     <Section id="features">
@@ -36,6 +39,27 @@ const Benefits = () => {
                   <Arrow />
                 </div>
               </div>
+
+              {benefit.light && <GradientLight />}
+
+              <div
+                className="absolute inset-0.5 bg-n-8"
+                style={{ clipPath: "url(#benefits)" }}
+              >
+                <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10 ">
+                  {benefit.imageUrl && (
+                    <img
+                      src={benefit.imageUrl}
+                      width={380}
+                      height={362}
+                      alt={benefit.title}
+                      className="w-full h-full object-cover"
+                    />
+                  )}
+                </div>
+              </div>
+
+              <ClipPath />
             </div>
           ))}
         </div>
